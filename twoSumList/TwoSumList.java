b62a0e2908ea47feb48843acd0afc8bc5b439725
Vincent Zheng <vincent.zheng@itsp-inc.com>
2024-07-25 18:02:04 -0400

    Adding the comment

--- twoSumList/TwoSumList.java
+++ twoSumList/TwoSumList.java
@@ -3,7 +3,7 @@ package vincent.leetcode.twoSumList;
 public class TwoSumList {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode resultNode = new ListNode();
-        ListNode tail = resultNode;
+        ListNode tail = resultNode; // using the extra node for moving
         int carryOn = 0;
         while (l1 != null || l2 != null || carryOn > 0) {
             int sum = (l1 == null ? 0 : l1.val) + (l2 == null ? 0 : l2.val) + carryOn;
